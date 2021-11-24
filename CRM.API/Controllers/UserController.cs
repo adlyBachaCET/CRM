6452ff1ee09c7c6c6090c8abacbcff3591cae756
Adly Bacha <adly.bacha@chambieagle.com>
2021-11-24 14:43:15 +0100

    ok

--- CRM.API/Controllers/UserController.cs
+++ CRM.API/Controllers/UserController.cs
@@ -388,9 +388,9 @@ namespace CRM_API.Controllers
         }
         /// <summary>This method updates .</summary>
         /// <param name="Id">Id of the BusinessUnit .</param>
-        [HttpPut("Photo/{Id}")]
+        [HttpPut("Photo")]
         public async Task<IActionResult> Photo([FromHeader(Name = "Token")][Required(ErrorMessage = "Token is required")]
-        string Token, int Id, IFormFile File)
+        string Token, IFormFile File)
         {
             var claims = _UserService.getPrincipal(Token);
             var Role = claims.FindFirst("Role").Value;
@@ -406,7 +406,7 @@ namespace CRM_API.Controllers
                     await File.CopyToAsync(stream);
                 }
            
-                await _UserService.UpdatePhoto(Id, "" + IdUser + ext);
+                await _UserService.UpdatePhoto(IdUser, "" + IdUser + ext);
                //claims.FindFirst("Photo").Value;
 
                 return StatusCode(StatusCodes.Status201Created);
